57b5a110295c4b4331008520487c469fe8c0cb26
Jeremy Dillow <jeremy.dillow@gmail.com>
2014-12-09 14:18:28 -0800

    Initial Commit

---
+++ GTM ToolsTests/GTM_ToolsTests.swift
@@ -0,0 +1,36 @@
+//
+//  GTM_ToolsTests.swift
+//  GTM ToolsTests
+//
+//  Created by Jeremy Dillow on 12/9/14.
+//  Copyright (c) 2014 Analytics Pros. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class GTM_ToolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
